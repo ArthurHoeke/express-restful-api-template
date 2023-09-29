5a81fcb3b8cee005d7a4a0cf6578180ae6d0d09b
Arthur <34348870+ArthurHoeke@users.noreply.github.com>
2023-09-29 15:16:31 +0200

    adjusted user model

--- app/Models/user.models.js
+++ app/Models/user.models.js
@@ -1,42 +1,49 @@
-const createUserTable = () => {
-    const sqlQuery = `
-        CREATE TABLE IF NOT EXISTS user (
-        id integer PRIMARY KEY AUTOINCREMENT,
-        email varchar UNIQUE,
-        password varchar,
-        role integer)`;
+const tableName = "user";
+
+const createTable = () => {
+    const columns = [
+        { name: 'id', type: 'integer PRIMARY KEY AUTOINCREMENT' },
+        { name: 'email', type: 'varchar UNIQUE' },
+        { name: 'password', type: 'varchar' },
+        { name: 'role', type: 'integer' }
+    ];
+
+    const columnDefinitions = columns.map(col => `${col.name} ${col.type}`).join(', ');
+
+    const sqlQuery = `CREATE TABLE IF NOT EXISTS ${tableName} (${columnDefinitions})`;
 
     return database.run(sqlQuery);
 }
 
-createUserTable();
+
+createTable();
 
 const create = (data, cb) => {
-    return database.run('INSERT INTO user (email, password, role) VALUES (?,?,?)', data, (err) => {
+    return database.run(`INSERT INTO ${tableName} (email, password, role) VALUES (?,?,?)`, data, (err) => {
         cb(err)
     });
 }
 
 const getUserCount = (cb) => {
-    return database.get('SELECT COUNT(*) AS userCount FROM user', (err, row) => {
+    return database.get(`SELECT COUNT(*) AS userCount FROM ${tableName}`, (err, row) => {
         cb(err, row)
     });
 }
 
 const getUserIdByEmail = (data, cb) => {
-    return database.get('SELECT id FROM user WHERE email = ?', data, (err, row) => {
+    return database.get(`SELECT id FROM ${tableName} WHERE email = ?`, data, (err, row) => {
         cb(err, row)
     });
 }
 
 const getUserEmailById = (data, cb) => {
-    return database.get('SELECT email FROM user WHERE id = ?', data, (err, row) => {
+    return database.get(`SELECT email FROM ${tableName} WHERE id = ?`, data, (err, row) => {
         cb(err, row)
     });
 }
 
 const getUserDataByEmail = (data, cb) => {
-    return database.get('SELECT id, password, role FROM user WHERE email = ?', data, (err, row) => {
+    return database.get(`SELECT id, password, role FROM ${tableName} WHERE email = ?`, data, (err, row) => {
         cb(err, row)
     });
 }
